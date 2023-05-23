aef6a3b9650fc9363c84c15e432cab73788770f3
Matt Groff <mattlgroff@gmail.com>
2023-05-23 00:58:30 -0400

    Cheerio

--- src/utils/navy-federal.ts
+++ src/utils/navy-federal.ts
@@ -1,35 +1,30 @@
 import { InterestRate } from '@deps/types';
-import chromium from 'chrome-aws-lambda';
+import { load } from 'cheerio';
 
 export async function fetchNavyFederal(): Promise<InterestRate[]> {
     try {
-        const browser = await chromium.puppeteer.launch({
-            args: chromium.args,
-            defaultViewport: chromium.defaultViewport,
-            executablePath: await chromium.executablePath,
-            headless: chromium.headless,
-            ignoreHTTPSErrors: true,
-        });
-        const page = await browser.newPage();
-        await page.goto('https://www.navyfederal.org/loans-cards/mortgage/mortgage-rates/conventional-fixed-rate-mortgages.html');
+        const response = await fetch(
+            'https://www.navyfederal.org/loans-cards/mortgage/mortgage-rates/conventional-fixed-rate-mortgages.html'
+        );
+        const body = await response.text();
 
-        const loanTerms: InterestRate[] = await page.$$eval('.table-resp tbody tr', rows => {
-            return rows.map(row => {
-                const columns = Array.from(row.querySelectorAll('td'));
-                const term = row.querySelector('th')?.textContent?.trim() || '';
-                const rate = parseFloat(columns[0]?.textContent?.replace('%', '') || '0');
-                const discountPoints = parseFloat(columns[1]?.textContent || '0');
+        const $ = load(body);
+        const loanTerms: InterestRate[] = [];
 
-                return {
-                    term: term,
-                    rate: rate,
-                    discountPoints: discountPoints,
-                    rateWithoutDiscount: rate + discountPoints * 0.25, // Assuming each discount point reduces the rate by 0.25%
-                };
+        $('.table-resp tbody tr').each((_, row) => {
+            const columns = $(row).find('td');
+            const term = $(row).find('th').text().trim();
+            const rate = parseFloat(columns.eq(0).text().replace('%', ''));
+            const discountPoints = parseFloat(columns.eq(1).text());
+
+            loanTerms.push({
+                term: term,
+                rate: rate,
+                discountPoints: discountPoints,
+                rateWithoutDiscount: rate + discountPoints * 0.25, // Assuming each discount point reduces the rate by 0.25%
             });
         });
 
-        await browser.close();
         return loanTerms;
     } catch (err) {
         console.error(err);
